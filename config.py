a1c01cd6049503447c6cd65e581d612d8366b027
Techlegacy7 <99082897+Techlegacy7@users.noreply.github.com>
2023-11-05 14:52:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6693351766:AAGf7r5Is4V3Q_pbOg2p73Vs7m-Fk6q9pqc")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6578974263:AAGAXkdt5oulpXFqJtzmaTMsQx-6OWY2bvE")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "25354498"))
@@ -22,7 +22,7 @@ API_HASH = os.environ.get("API_HASH", "1cdcf1a33fc5108fa3ca151480eece50")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001941605552"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "1969177696"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1969177696", "1163343128"))
 
 #Port
 PORT = os.environ.get("PORT", "8081")
@@ -32,12 +32,12 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://gs0478717:X3R9cW7hVv9YjDy
 DB_NAME = os.environ.get("DATABASE_NAME", "FILESBOT")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001257359849"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001192753359"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "👋 Hello {first}! Welcome to PrimeMods Files Bot! \n\n🌟Join our main channel @PrimeModsApps for more! 📺🌸.")
+START_MSG = os.environ.get("START_MESSAGE", "👋 Hello {first}! Welcome to zEdFiles Bot! \n\n🌟Join our main channel @Hackinsider for more! .")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -46,19 +46,19 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join my Channel in order to use me\n\nKindly join @iAnimeHub and it\'s private channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join my Channel in order to use me\n\nKindly join @Hackinsider and it\'s private channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "      🌀JOIN FOR MORE🌀\n          @HACKINSIDER")
 
 #set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "Hello there! \n\nPlease do not send me any direct message as I can only send you files once you click on any link.\n\nJoin @iAnimeHub for more anime fun! 🌟🌸 Explore a wide variety of shows and connect with fellow enthusiasts. "
+USER_REPLY_TEXT = "Hello there! \n\nPlease do not send me any direct message as I can only send you files once you click on any link.\n\nJoin @iAnimeHub for more @HACKINSIDER and connect with fellow enthusiasts. "
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1969177696)
